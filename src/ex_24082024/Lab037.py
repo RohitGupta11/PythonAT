3da974b70962e105c86149f3cd04113a42396408
Rohit Gupta <rg110298@gmail.com>
2024-08-20 22:36:23 +0530

    adding 20aug files

---
+++ src/ex_24082024/Lab037.py
@@ -0,0 +1,8 @@
+#write a program to take a user age and let him know if he van go to the club.
+
+age=int(input("enter the age:"))
+
+if age>=21:
+    print(f"He can go to the club as age is:{age}")
+else:
+    print(f"He can't to the club as age is:{age}")
---
+++ src/ex_24082024/Lab038.py
@@ -0,0 +1,8 @@
+#a=10 this is assignment
+#a==10 --> this is true or false
+
+a=9
+if a==10:
+    print("Hello")
+else:
+    print("not hello")
